dfdd0fbbc7baec2cc952f7034f66a7fa55400cab
de-graeuler <bernhard@graeuler.de>
2016-11-28 00:29:26 +0100

    removing unused imports

--- monitor/src/main/java/de/graeuler/garden/monitor/service/NetworkTrafficMonitorService.java
+++ monitor/src/main/java/de/graeuler/garden/monitor/service/NetworkTrafficMonitorService.java
@@ -3,7 +3,7 @@ package de.graeuler.garden.monitor.service;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
--- monitor/src/main/java/de/graeuler/garden/monitor/service/SensorMonitorService.java
+++ monitor/src/main/java/de/graeuler/garden/monitor/service/SensorMonitorService.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
