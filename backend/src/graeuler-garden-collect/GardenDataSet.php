0515a0d200096ff321a1ba1f01b20697c969edcc
de-graeuler <bernhard@graeuler.de>
2016-12-06 12:22:26 +0100

    Testing the GardenDataSet. Improvements.

--- backend/src/graeuler-garden-collect/GardenDataSet.php
+++ backend/src/graeuler-garden-collect/GardenDataSet.php
@@ -11,7 +11,9 @@ class GardenDataSet implements DataSet
      * @param mixed - source, key, isodatetime, value
      **/
     public function addValue($mixed) { // in PHP 5.6 we should refactor this to ...$mixed
-        $this->records[] = func_get_args();
+        $args = func_get_args();
+        if (sizeof($args) == 4) 
+            $this->records[] = $args;
     }
  
     public function asIterableList() {
